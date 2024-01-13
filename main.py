e0ee3bffe135308b2b64bef6bbf76132cdbfd0ce
Milad Alkhamis <mi-alkhamis@hotmail.com>
2024-01-13 16:43:22 +0330

    Walk through DB files and execute query on them

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sqlite3
+import os
 
 # ------------------------------  Configuration  ------------------------------
 DEVICE_ID = {
@@ -22,3 +24,46 @@ DEVICE_ID = {
 }
 DB_PATH = "cmitech"
 EXPORT_PATH = "export"
+# ------------------------------  READ DB Files  ------------------------------
+
+
+def walk(db_path):
+    """
+    Read All DB files in path and return DB file and DeviceID
+
+    Args:
+        db_path (str): The path to the directory containing the DB files.
+    """
+    for dir_path, _, filenames in os.walk(db_path):
+        if not filenames:
+            continue
+        if "ServiceLog.db" not in filenames:
+            print(f"'ServiceLog.db' not found in '{dir_path}'.")
+            continue
+        db_path = os.path.join(dir_path, "ServiceLog.db")
+        # normilize path and split it and put device folder name in device_num
+        device_num = os.path.normpath(db_path).split(os.sep)[1]
+        read_db(db_path, DEVICE_ID.get(device_num))
+
+
+def read_db(db_path, deviceID):
+    """
+    Connect DB and run query to filter proper data.
+
+    Args:
+        db_path (str): The path to the DB file.
+        deviceID (str): The device ID associated with the DB file.
+    """
+    connection = sqlite3.connect(db_path)
+    query = f"""
+                SELECT Timestamp,UserUID from  event_log
+                WHERE EventType = "Recognition"
+                AND AdditionalData = "Allowed"
+				order by Timestamp
+            """
+    cursor = connection.cursor()
+    cursor.execute(query)
+
+
+# ------------------------------  MAIN  ------------------------------
+walk(DB_PATH)
