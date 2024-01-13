28a37a9213c4f610aa2ab7c3dd3367b53e878c14
Milad Alkhamis <mi-alkhamis@hotmail.com>
2024-01-13 16:48:48 +0330

    Convert CMI-TECH EF45 timestamp to RAYA timestamp

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import sqlite3
 import os
 
@@ -63,6 +64,46 @@ def read_db(db_path, deviceID):
             """
     cursor = connection.cursor()
     cursor.execute(query)
+    create_txt_file(cursor.fetchall(), deviceID)
+# ------------------------------  Convert  ------------------------------
+
+
+def create_txt_file(raw_data, deviceID):
+    """Convert CMI-TECH EF-45 Enterance Log to RAYA  format
+    3120110223160100010000000301000118
+    31 2011-02-23 16:01 0001 0000000301 0001 18
+    31 date time Enter_type userID device_ID 18
+
+    Args:
+        raw_data (list): A list of tuples containing the timestamp and user UID from the DB query.
+        deviceID (str): The device ID associated with the DB file.
+    """
+    if not os.path.exists(EXPORT_PATH):
+        os.mkdir(EXPORT_PATH)
+    export_file = f"{EXPORT_PATH}\{deviceID}.txt"
+    with open(export_file, "w") as file:
+        # Use list comprehensions to create lists from existing iterables
+        for row in raw_data:
+            userID = row[1].strip().zfill(10)
+            timestamp = convert_timestamp(row[0])
+            log_sequence = f"31{timestamp}0003{userID}{deviceID}18"
+            file.write(log_sequence)
+            file.write("\n")
+
+
+def convert_timestamp(timestamp):
+    """Convert CMI-TECH EF-45 Enterance Log Time format(2023-02-15T13:08:19Z)
+    to RAYA  format(202302151308).
+
+    Args:
+        timestamp (str): The timestamp in CMI-TECH EF-45 Enterance Log Time format.
+
+    Returns:
+        str: The timestamp in RAYA format.
+    """
+
+    dt_object = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%SZ")
+    return dt_object.strftime("%Y%m%d%H%M")
 
 
 # ------------------------------  MAIN  ------------------------------
